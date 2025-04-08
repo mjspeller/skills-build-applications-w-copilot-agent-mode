e032971cfa8765c343d3b6613a78f6a205d4a2df
Mark Speller <mjspeller@hotmail.com>
2025-04-08 13:02:42 +0000

    Refactor database population command and enhance app initialization debug output

--- octofit-tracker/backend/octofit_tracker/apps.py
+++ octofit-tracker/backend/octofit_tracker/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class OctofitTrackerConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "octofit_tracker"
+
+    def ready(self):
+        print('Debug: octofit_tracker app is ready and initialized.')
--- octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -1,36 +1,41 @@
 from django.core.management.base import BaseCommand
 from octofit_tracker.models import User, Team, Activity, Leaderboard, Workout
+from django.conf import settings
+from pymongo import MongoClient
 from datetime import timedelta
 from bson import ObjectId
 
 class Command(BaseCommand):
-    help = 'Populate the database with test data for users, teams, activities, leaderboard, and workouts'
+    help = 'Populate the database with test data for users, teams, activity, leaderboard, and workouts'
 
     def handle(self, *args, **kwargs):
-        # Clear existing data
-        User.objects.all().delete()
-        Team.objects.all().delete()
-        Activity.objects.all().delete()
-        Leaderboard.objects.all().delete()
-        Workout.objects.all().delete()
+        self.stdout.write('Debug: populate_db command is being executed.')
+        # Connect to MongoDB
+        client = MongoClient(settings.DATABASES['default']['HOST'], settings.DATABASES['default']['PORT'])
+        db = client[settings.DATABASES['default']['NAME']]
+
+        # Drop existing collections
+        db.users.drop()
+        db.teams.drop()
+        db.activity.drop()
+        db.leaderboard.drop()
+        db.workouts.drop()
 
         # Create users
         users = [
             User(_id=ObjectId(), username='thundergod', email='thundergod@mhigh.edu', password='thundergodpassword'),
             User(_id=ObjectId(), username='metalgeek', email='metalgeek@mhigh.edu', password='metalgeekpassword'),
             User(_id=ObjectId(), username='zerocool', email='zerocool@mhigh.edu', password='zerocoolpassword'),
-            User(_id=ObjectId(), username='crashoverride', email='crashoverride@mhigh.edu', password='crashoverridepassword'),
+            User(_id=ObjectId(), username='crashoverride', email='crashoverride@hmhigh.edu', password='crashoverridepassword'),
             User(_id=ObjectId(), username='sleeptoken', email='sleeptoken@mhigh.edu', password='sleeptokenpassword'),
         ]
         User.objects.bulk_create(users)
 
         # Create teams
-        team1 = Team(_id=ObjectId(), name='Blue Team')
-        team2 = Team(_id=ObjectId(), name='Gold Team')
-        team1.save()
-        team2.save()
-        team1.members.add(*users[:3])
-        team2.members.add(*users[3:])
+        team = Team(_id=ObjectId(), name='Blue Team')
+        team.save()
+        for user in users:
+            team.members.add(user)
 
         # Create activities
         activities = [
